5a47c29a4723b9f0c7509d5b7efd41065f8446fc
sakib213 <sakibhss438@gmail.com>
2023-03-02 21:31:19 +0600

    like

--- index.js
+++ index.js
@@ -36,10 +36,13 @@ function verifyToken(req, res, next) {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w8b4lyk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const productsCollection = client.db('technical-product').collection('products');
     const userCollection = client.db('technical-product').collection('user');
     const orderCollection = client.db('technical-product').collection('collectOrder');
